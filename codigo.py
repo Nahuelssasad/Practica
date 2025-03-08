f3ec38bcca271dd186d5e1988785d8ca61f95d95
Nicolas <nicolas.spinoza30@gmail.com>
2025-03-08 20:07:53 -0300

    Union hecha

--- codigo.py
+++ codigo.py
@@ -1,8 +1,9 @@
+"""Cambio de turno"""
 nombre = "Nicolas"
 apellido = "Spinoza"
 
 saludo = "Hola "+ nombre + " " + apellido + " ¿Como estas?"
-print(saludo + "LALALALAL")
+print(saludo + "JAJAJAJA")
 
 pablo = 40
 print(pablo)
