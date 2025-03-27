551fa214fd606402c3fde450734a4dfa097b9b61
Nicolas <nicolas.spinoza30@gmail.com>
2025-03-27 10:50:29 -0300

    Trabajando struct

---
+++ struct.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define  CAPACITY  50 
+typedef struct queues
+{
+    int matriz[CAPACITY];
+    int size;
+} queues;
+
+int main(void)
+{
+    //declarando una estructura tipo queues
+    queues people  ;
+    people.matriz[0] = 10;
+    printf("%d",people.matriz[0]);
+}
