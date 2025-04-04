b52de9957b1131ef4dd38894fd15fa19ebf8d8fa
Nicolas <nicolas.spinoza30@gmail.com>
2025-04-04 08:48:42 -0300

    MergeSort

---
+++ mergeSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+//prototipos de funciones
+void merge(int array[],int izq,int mid,int der);
+void mergeSort(int array[],int izq,int der);
+
+
+int main(void)
+{
+
+}
+//definiendo las funciones
+
+//union de submatrices a partir de array
+//primer arreglo sera de 
+void merge(int array[],int izq,int mid,int der)
+{
+    
+    //variables que  comparen  las dos submatrices
+    int i = izq;
+    int j = mid+1;
+    int k =izq;
+    //
+}
+
+void mergeSort(int array[],int izq,int der)
+{
+    int mid = (izq+der)/2;
+    mergeSort(array,izq,der);
+    mergeSort(array,mid+1,izq);
+    merge(array,izq,mid,der);
+
+}
