23788f8ffa3c63d6d1b648a704ca808625e626c9
Nicolas Spinoza <159769899+Nahuelssasad@users.noreply.github.com>
2025-04-05 17:21:32 -0300

    Error en codigo C

--- codigo.c
+++ codigo.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
 
-int main(void)
+//Declaracion de  las funciones
+
+merge(int A[],int lowerLimit,int mid,int upperLimit)
 {
-    
-    int matriz [] = {4,5,8,12};
-    //aplicamos busqueda binaria para ordenar los elementos
-    
-    for(int i = 0;i<4;i++)
+    //Declaracion y definicions  de los  punteros para comparar las submatrices
+    int i = lowerLimit;
+    int j = mid+1;
+    int k = lowerLimit;
+
+    int arrayAuxiliar[upperLimit+1];
+
+    //Comparando elementos de las submatrices
+    while((i < mid) && (j < upperLimit))
     {
-       
-        int temp = 0;
-        for(int j = 0;j < 4 ; j++)
+
+        ifA[i] <= A[j]
         {
-            if(matriz[j] > matriz[j+1])
-            {
-                temp = matriz[j];
-                matriz[j] = matriz[j+1];
-                matriz[j+1] = temp;
-              
-            }
-            
+            arrayAuxiliar[k] = A[i];
         }
+
+        else{
+
+        }
+        k++;
+
+
     }
 
-    //imrpimiendo lista ordenada a traves del metodo bubble sort
-    for(int j = 0 ; j<4;j++)
-    {
-        printf("%i ",matriz[j]);
-    }
-    printf("HOLA ME LLAMO NICO")
+     
+
+
+
+
+};
+
+void mergeSort(int A[],int lowerLimit,int upperLimit) //rigth : Limite inferior ; Left : Limite superior
+{
+    int mid = (lowerLimit + upperLimit)/2;  //Nos posicionamos en el medio de la matriz
+    mergeSort(A,lowerLimit,mid);  //Recursivamente vamos creando submatrices
+    mergeSort(A,mid+1,upperLimit);
+    merge(A,lowerLimit,mid,upperLimit); //Unimos las submatrices en una sola matriz ordenada
+
+} 
+
+int main(void)
+{
 
 
 }
